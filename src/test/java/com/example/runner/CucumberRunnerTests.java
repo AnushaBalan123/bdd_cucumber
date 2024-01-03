0c8843a96ebc6c3c5a2383f51117ed0acc2751e9
Vibha Singh <vibhasingh2004@gmail.com>
2024-01-03 15:29:50 +0000

    Update dependencies and ReadMe

--- src/test/java/com/example/runner/CucumberRunnerTests.java
+++ src/test/java/com/example/runner/CucumberRunnerTests.java
@@ -1,2 +1,11 @@
-package com.example.runner;public class CucumberRunnerTests {
+package com.example.runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(tags = "", features = {"src/test/resources/features/LoginPage.feature"}, glue = {"com.example.definitions"},
+        plugin = {})
+
+public class CucumberRunnerTests extends AbstractTestNGCucumberTests {
+
 }
